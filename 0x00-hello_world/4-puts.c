6fa17546b3423df0f134e1e80367af3ba0a0a581
Peacechimamka <peaceamaka111@gmail.com>
2022-09-10 04:20:11 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-
 /**
  *main - Entry point
+
  *Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	puts("\"Progamming is like building a multilingual puzzle");
+	printf("\"Progamming is like building a multilingual puzzle");
 	return (0);
 }
