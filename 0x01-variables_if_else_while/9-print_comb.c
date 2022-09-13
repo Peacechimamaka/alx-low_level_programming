c09fdccb690b3dd15eb1826826d234440049d593
Peacechimamka <peaceamaka111@gmail.com>
2022-09-12 17:58:27 -0700

    comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-/*
+
+/**
  * main - prints all the numbers of base 16 in lowercase.
  *
  * Return: Always 0
@@ -7,7 +8,7 @@
 int  main(void)
 {
 	int num;
-	int letter;
+	char letter;
 
 	for (num = 0; num < 10; num++)
 	{
