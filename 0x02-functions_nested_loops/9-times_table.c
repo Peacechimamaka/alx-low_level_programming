67685dbd257663ef31c721d729537f1875e29322
Peacechimamka <peaceamaka111@gmail.com>
2022-09-14 21:21:17 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,11 +4,11 @@
 *
 * Return: Always 0.
 */
-void times_table(void)  
+void times_table(void)
 {
 int a;
 int b;
-int c;  
+int c;
 for (a = 0; a <= 9; a++)
 {
 for (b = 0; b <= 9; b++)
@@ -35,7 +35,7 @@ else
 {
 _putchar ((c / 10) + '0');
 _putchar ((c % 10) + '0');	      
-if (b < 9)		
+if (b < 9)	
 {
 _putchar(',');
 _putchar(' ');
@@ -43,5 +43,5 @@ _putchar(' ');
 }
 }
 _putchar ('\n');
-} 
+}
 }
