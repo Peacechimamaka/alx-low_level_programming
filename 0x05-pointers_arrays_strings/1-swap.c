76fdc5c01d402ae8de6e613f8e26c6467722e8ca
Peacechimamka <peaceamaka111@gmail.com>
2022-09-20 15:20:52 -0700

    swapping

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+nclude "main.h"
+
+/**
+ *
+ *  * swap_int - swaps value
+ *
+ *   * @a: params
+ *
+ *    * @b: params
+ *
+ *     */
+void swap_int(int *a, int *b)
+{
+int tmp = *a;
+*a = *b;
+*b = tmp;
+}
+
