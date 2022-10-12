e03d3a14fe8688152b563ab044139dfe8846d2bf
Peacechimamaka <peaceamaka111@gmail.com>
2022-10-12 07:39:58 -0700

    init

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,7 +9,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-		d.name = "jimmy"
-		d.age = 3.0
-		d.owner = "Elais"
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+}
 }
