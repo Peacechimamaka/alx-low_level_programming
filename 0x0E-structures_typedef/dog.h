09c3b27529ea8ede402dab27c7281acf4d41eeba
Peacechimamaka <peaceamaka111@gmail.com>
2022-10-12 05:42:19 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,23 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef _DOG_H
+#define _DOG_H
+
 /**
- * main - a function that returns a structure value.
- * struct: used to creat a structure function in c
- * Return: 0
+ * struct dog - a function that returns a structure value.
+ * @name: the dog's name
+ * @age: the age of the dog
+ * @owner: the owner of the dog
  */
-typedef struct dog dog_t;
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-int main()
-{
-	dog_t mydog = {"Elias", 3, "Betty"};
-
-	printf ("my dog's name is %s.\n", mydog.name);
-	printf ("my dog's age is %f,\n", mydog.age);
-	printf ("my dog owner is %s,\n", mydog.owner);
-	return 0;
-}
+#endif
