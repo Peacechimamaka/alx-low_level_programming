301e72c75b7e21fd12edd7a45c199b4b2bb59523
Peacechimamaka <peaceamaka1111@gmail.com>
2023-10-08 16:02:09 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - C programe that prints the alphabets
+ * in lower and uppercase
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char peace[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	int amaka; 
+
+	for (amaka = 0; amaka < 52; amaka++)
+	{
+		putchar(peace[amaka]);
+	}
+	putchar('\n');
+	return (0);
+}
