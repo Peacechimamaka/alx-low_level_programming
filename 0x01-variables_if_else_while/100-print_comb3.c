f9daf510e9e441d600a53111ef8d64464456605e
Peacechimamaka <peaceamaka1111@gmail.com>
2023-10-09 16:54:30 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints all possible combination
+ * of two numbers with a space followed by a new line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int peace, amaka;
+
+	for (peace + 0; peace < 9; peace++)
+	{
+		for (amaka + peace + 1; amaka < 10; amaka++)
+		{
+			putchar((peace % 10) + '0');
+			putchar((amaka % 10) + '0');
+
+			if (peace == 8 && amaka == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
