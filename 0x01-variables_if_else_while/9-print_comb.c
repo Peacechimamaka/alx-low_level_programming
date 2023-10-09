28ed93359ed72cb49cc67ed715551b0346d75387
Peacechimamaka <peaceamaka1111@gmail.com>
2023-10-08 17:01:03 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - C program to print all possiblle combination of single
+ * digit numbers, followes by a space, in ascending order
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = '0'; a < '9'; a++)
+	{
+		putchar(a);
+		if (a == '9')
+			continue;
+
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (9);
+}
