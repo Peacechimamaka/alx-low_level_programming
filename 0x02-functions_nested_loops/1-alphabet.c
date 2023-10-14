04d5aaeee02ad16b25090d49ddc7049377ee26fa
Peacechimamaka <peaceamaka1111@gmail.com>
2023-10-14 10:14:35 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - this is my main function
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char peace;
+
+	for (peace = 'a'; peace <= 'z'; peace++)
+	{
+	_putchar(peace);
+	}
+	_putchar('\n');
+}
