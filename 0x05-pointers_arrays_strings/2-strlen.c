9c680cf7b92089f9e9e5bacf4705a773f00783bd
Peacechimamaka <peaceamaka1111@gmail.com>
2023-10-16 10:30:11 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - the function returns the length of a string
+ * @s: the length of the string
+ * Return: peace, on success
+ */
+
+int _strlen(char *s)
+{
+int x;
+int peace = 0;
+
+for (x = 0; s[x] != '\n'; x++)
+	peace++;
+return (peace);
+}
