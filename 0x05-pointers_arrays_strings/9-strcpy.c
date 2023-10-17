dbc8e681d5facb1993cfc9872cccacfc7c399a84
Peacechimamaka <peaceamaka1111@gmail.com>
2023-10-16 22:03:45 -0700

    9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - the function that prints
+ * @dest: ist arg
+ * @src: 2nd arg
+ * Return: 0
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int x;
+
+	for (x = 0; src[x] != '\0'; x++)
+	{
+		dest[x] = src[1];
+	}
+	dest[x] = '\0';
+	return (dest);
+}
