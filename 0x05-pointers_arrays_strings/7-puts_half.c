85b475f2eebf9d989e4e5ee9eebe13b880bb9eec
Peacechimamaka <peaceamaka1111@gmail.com>
2023-10-16 21:22:23 -0700

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts_half - the function that prints half of the sting
+ * @str: the arguement of the function
+ */
+
+void puts_half(char *str)
+{
+	int x;
+	int y;
+	int peace = 0;
+
+	for (x = 0; str[x] != '\n'; x++)
+	{
+		peace++;
+	}
+	y = (peace - 1) / 2;
+	for (x = y + 1; str[x] != '\0'; x++)
+	{
+		_putchar(str[x]);
+	}
+	_putchar('\n');
+}
