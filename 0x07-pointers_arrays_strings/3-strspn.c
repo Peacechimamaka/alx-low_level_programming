8b556a2d8be918181023479b277777c27665f0f6
Peacechimamaka <peaceamaka1111@gmail.com>
2023-10-23 14:45:08 -0700

    3-sprspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+  * _strspn - function name
+  * @s: parameter
+  * @accept: parameter
+  * Return: 0.
+  */
+
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int _strspn(char *s, char *accept);
+unsigned int y = 0;
+int x;
+
+while (*s)
+{
+for (x = 0; accept[x]; x++)
+{
+if (*s == accept[x])
+{
+y++;
+break;
+}
+else if (accept[x + 1] == '\0')
+return (y);
+}
+s++;
+}
+return (y);
+
+}
