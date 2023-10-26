c6e672f4b33bebb2274c615a2ead7cf626bfeec5
Peacechimamaka <peaceamaka1111@gmail.com>
2023-10-26 12:15:28 -0700

    string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - a recursive function
+ * @s: the parameter
+ * Return: always 0
+ */
+
+void _puts_recursion(char *s)
+{
+	if(*s)
+	{
+		_putchar(*s);
+			_puts_recursion(s++);
+	}
+	else
+	_putchar('\n');
+}
