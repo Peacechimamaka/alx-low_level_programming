f6a37adfe296f3f95c06180668d7272245f9705e
Peacechimamaka <peaceamaka1111@gmail.com>
2023-10-26 15:41:21 -0700

    strlent

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,16 +4,14 @@
  * @s: parameter
  * Return: count
  */
-
-int _strlen_recursion(char *s);
+int _strlen_recursion(char *s)
 {
 	int count = 0;
 
 	if (*s)
 	{
 		count++;
-		strlen_recursion(s + 1);
-		count += strlen_recursion(s + 1);
+		count += _strlen_recursion(s + 1);
 	}
 	return (count);
 }
