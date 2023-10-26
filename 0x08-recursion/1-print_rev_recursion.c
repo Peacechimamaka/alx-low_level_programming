35d6b2e32e6ed8faa5891b45f9350af5fc830007
Peacechimamaka <peaceamaka1111@gmail.com>
2023-10-26 12:42:32 -0700

    rev

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - the main function
+ * @s: the function parameter
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar (*s);
+	}
+	_putchar ('\n');
+}
