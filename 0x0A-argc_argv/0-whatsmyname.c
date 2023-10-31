99191dfe2e9631bf68e51977c5d14ffe6e75d126
Peacechimamaka <peaceamaka1111@gmail.com>
2023-10-30 21:39:50 -0700

    argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - the main funxtion
+ *@argc: for tge nber of args
+ *@argv: for the argument itself
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
