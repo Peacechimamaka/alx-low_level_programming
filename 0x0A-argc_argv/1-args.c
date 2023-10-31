c9997c97c206f261630fbddb97f72f060063f3f0
Peacechimamaka <peaceamaka1111@gmail.com>
2023-10-30 23:28:23 -0700

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - the main function that recieves an arguement
+ * argc: the number of arguments
+ * argv: the arguments
+ * Return: 0
+ */
+int main(int argc, char* argv[])
+{
+	(void) argv[0];
+
+		printf("%d\n", argc);
+	return (0);
+}
