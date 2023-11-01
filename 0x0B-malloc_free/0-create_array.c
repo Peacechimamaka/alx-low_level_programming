a172885d4a01edb28babade05f655942353d2bb2
Peacechimamaka <peaceamaka1111@gmail.com>
2023-11-01 14:09:58 -0700

    array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <main.h>
+/**
+*create_array - the function name
+*@size: the size of the array
+*@c: the character
+*Return: 0
+*/
+
+char *create_array(unsigned int size, char c)
+{
+char *ptArr;
+unsigned int length;
+
+ptArr = malloc(size * sizeof(char));
+for (length = 0; length < size; length++)
+{
+ptArr[count] = c;
+}
+return (ptArr);
+}
+
