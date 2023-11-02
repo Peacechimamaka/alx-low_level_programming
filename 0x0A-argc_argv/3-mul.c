46bafbb9b383f06ddbabb7b26a3f01ff94a0c63d
Peacechimamaka <peaceamaka1111@gmail.com>
2023-11-01 17:36:57 -0700

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the function
+ *@argc: number of arguements
+ *@argv: the arguements
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int mul;
+	int num1, num2;
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	mul = num1 * num2;
+	if (argc == 3)
+	{
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
