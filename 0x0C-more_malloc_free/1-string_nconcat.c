b46cefb706ebf6d7c596a4df433d144ee4d12557
Peacechimamaka <peaceamaka1111@gmail.com>
2023-11-03 03:51:46 -0700

    concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,21 +9,21 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int count;
-	int lens1 = 0;
-	int lens2 = 0;
+	unsigned int count;
+	unsigned int lens1 = 0;
+	unsigned int lens2 = 0;
 	char *result;
 
 	if (s1 == NULL)
 	s1 = "";
 	if (s2 == NULL)
-	s2 == "";
-	for (count = 0; s[1] != '\0'; count++)
+	s2 = "";
+	for (count = 0; s1[count] != '\0'; count++)
 	lens1++;
 	for (count = 0; s2[count] != '\0'; count++)
-	len2++;
+	lens2++;
 	result = malloc(sizeof(char) * (lens1 + n) + 1);
-	if (result0 == NULL)
+	if (result == NULL)
 	return (NULL);
 	if (n >= lens2)
 {
@@ -31,14 +31,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		result[count] = s1[count];
 	for (count = 0; s2[count] != '\0'; count++)
 		result[lens1 + count] = s2[count];
-	result[lens2 + count] = '\0';
+	result[lens1 + count] = '\0';
 }
 else
 {
-	for (count = 0; s[count] != '\0'; count++)
+	for (count = 0; s1[count] != '\0'; count++)
 		result[count] = s1[count];
 	for (count = 0; count < n; count++)
-		result[lens1 + count] = s2
+		result[lens1 + count] = s2[count];
 				result[lens1 + count] = '\0';
-				}
-				}
+}
+return (result);
+}
