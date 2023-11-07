268b3ef39a6da7ffb95543353654cafcb16882da
Peacechimamaka <peaceamaka1111@gmail.com>
2023-11-07 15:01:22 -0800

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+/**
+ * struct dog - functiin
+ * @name: name of
+ * @age: age of
+ * @owner:owner of the dog
+ * Return: 0
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
