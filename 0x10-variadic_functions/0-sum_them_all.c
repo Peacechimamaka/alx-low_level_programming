a95096fcb9c59b90c7287cffdad8a2c700ec3377
Peacechimamaka <peaceamaka1111@gmail.com>
2023-11-10 09:07:47 -0800

    sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,14 +13,14 @@ int sum_them_all(const unsigned int n, ...)
 	va_list sum;
 
 	va_start(sum, n);
-
+	
+	if (n == 0)
+		return (0);
 	for (i = 0; i < n; i++)
 	{
 		result = result + va_arg(sum, int);
 	}
-	if (n == 0)
-		return (0);
-	else
+	
 	va_end(sum);
 
 	return (result);
