5f4aa4911a2ea4a882df8ae116de5ae2a48275a7
Peacechimamaka <peaceamaka111@gmail.com>
2023-11-15 00:39:20 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet ten times
+ *
+ * Return : 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int x;
+	int count = 0;
+
+	while (count <= 10)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar (x);
+		};
+		count++;
+		_putchar ('\n');
+	}
+
+}
