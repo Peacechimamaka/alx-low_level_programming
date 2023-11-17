e1bb22cba4eea32c06e4f7f46a1a3a6e59487272
Peacechimamaka <peaceamaka111@gmail.com>
2023-11-17 14:48:40 -0800

    add node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *add_node - funcion tyat adds a new node
+ *@head: a pointer to thefirst node
+ *@str: the stromg
+ *Return: the address of the newnode
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newnode;
+
+	size_t count, x;
+
+	count = 0;
+
+	newnode = malloc(sizeof(list_t));
+	if (newnode == NULL)
+		return (NULL);
+	newnode->str = strdup(str);
+	newnode->next = *head;
+	*head = newnode;
+
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		count++;
+	}
+		newnode->len = count;
+	return (newnode);
+}
+
+
