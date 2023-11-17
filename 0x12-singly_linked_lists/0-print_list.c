cea2f985b7727797984ecb887d8408f8ea309898
Peacechimamaka <peaceamaka111@gmail.com>
2023-11-17 02:51:16 -0800

    print element

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,18 +5,19 @@
  *print_list - a function that prints a list
  *@h : the head pointer
  *
- *Return : the length of the node
+ *Return: the length of the node
  */
 size_t print_list(const list_t *h)
 {
 	size_t node_num;
+
 	node_num = 0;
 	while (h != NULL)
 	{
-		if (h-> str == NULL)
-			printf ("[%d] %s\n", 0, "(nil)");
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
 		else
-			printf ("[%d] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		node_num++;
 	}
