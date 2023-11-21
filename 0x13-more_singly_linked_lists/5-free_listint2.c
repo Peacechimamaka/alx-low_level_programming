9247275c5bdbb93f652166bf9139ce30cae9f38c
Peacechimamaka <peaceamaka111@gmail.com>
2023-11-21 15:55:06 -0800

    free a head

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ *free_listint2 - a function that frees head
+ *@head: the head pointer
+ *
+ *Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	if (*head == NULL)
+		return;
+
+	while (*head != NULL)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+}
