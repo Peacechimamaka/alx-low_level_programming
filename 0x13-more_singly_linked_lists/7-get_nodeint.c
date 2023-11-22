ed78848dbbf2eece88c0e169692aa3fdc2c23e98
Peacechimamaka <peaceamaka111@gmail.com>
2023-11-22 06:09:22 -0800

    indexing

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ *get_nodeint_at_index - a function that returns a node at a spexific index
+ *
+ *@index: the given index 
+ *
+ *Return: a pointer
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+	while (head != NULL)
+	{
+		if (count == index)
+		{
+			return(head);
+		}
+		head = head->next;
+		count++;
+	}
+	return (NULL);
+}
