533dc1200ffcc12ee2459fecaee252885147b90a
Peacechimamaka <peaceamaka111@gmail.com>
2024-02-02 07:32:21 -0800

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,10 +1,10 @@
 #include "hash_tables.h"
 /**
- *hash_table_create - a function that creates a table
+ * hash_table_create - a function that creates a table
  *
  * @size : the table size
  *
- * Return : a pointer, on success
+ *Return: a pointer, on success
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
@@ -18,7 +18,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	table->size = size;
-	table->array = calloc(size, sizeof(hash_node_t*));
+	table->array = calloc(size, sizeof(hash_node_t *));
 
 	if (table->array == NULL)
 	{
